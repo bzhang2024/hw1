0b2a21bc101388736188122dd39188aa42bb26c6
Blake Zhang <bzhang2024@gmail.com>
2025-01-31 14:38:23 -0800

    added testing code

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,63 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+
+using namespace std; 
 
 int main(int argc, char* argv[])
 {
+
+    Node* head = NULL;
+    Node* temp = NULL;
+
+    //sorted linked list 1 to i
+    for(int i = 1; i <= 5; i++) {
+        if(head == NULL){
+            head = new Node(i, NULL);
+            temp = head;
+        } 
+        else {
+            temp->next = new Node(i, NULL);
+            temp = temp->next;
+        }
+    }
+
+    Node* odds = NULL;
+    Node* evens = NULL;
+    
+    split(head, odds, evens);
+
+
+    //PRINTING STUFF OUT
+    cout << "odd numbers: "; 
+    temp = odds;
+    while(temp != NULL) {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl; 
+
+
+    cout << "Even numbers: ";
+    temp = evens;
+    while(temp != NULL) {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl; 
+
+    //deallocate mem
+    while(odds != NULL) {
+        Node* temp = odds;
+        odds = odds->next;
+        delete temp;
+    }
+    while(evens != NULL) {
+        Node* temp = evens;
+        evens = evens->next;
+        delete temp;
+    }
 
 }
